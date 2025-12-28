220a7dbc41cb4956a146bf45cffe32eff82c21a0
shafiul <shafiul7sms@gmail.com>
2025-12-28 09:36:57 +0600

    I leant some python string methos

---
+++ app.py
@@ -0,0 +1,11 @@
+course = 'python programming language' #This is a python programming language. will will learn python string methods and apply them in our code.
+
+print(course.upper())  # Convert to uppercase
+print(course.lower())  # Convert to lowercase
+print(course.title())  # Convert to title case
+
+print(course.find('lan'))  # Find substring
+print(course.replace('python', 'JavaScript'))  # Replace substring
+
+print('programming' in course)  # Check substring presence it return boolean value as like as True or False
+print('java' in course)  # Check substring presence it return boolean value as like as True or False
