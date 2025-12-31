8e10d989d78b29bd94c4fa87b3bd97db143f3a12
shafiul <shafiul7sms@gmail.com>
2025-12-31 08:42:23 +0600

    Learn Ternary operator

--- problem/day1.py
+++ problem/day1.py
@@ -1,2 +1,25 @@
-name = input('enter your name')
-print('hello,', name)
+ans = 10 <= 5
+
+print(ans)  # This will print: True
+
+
+letter = ord('a') == ord('A')
+
+print(letter)  # This will print: False
+
+
+
+#python if else condition and ternary operator
+age = 22
+
+if age >= 18:
+  message = "Eligiable"
+else:
+  message = "Not Eligiable"
+  
+print(message)
+
+
+message1 = "Eligiable" if age >= 18 else "Not Eligiable"
+
+print(message1)
