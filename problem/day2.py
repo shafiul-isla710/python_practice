25ca940f203486b399a5bed7c50674a9b011e1ed
shafiul <shafiul7sms@gmail.com>
2026-02-17 22:58:55 +0600

    I solved check odd and even number

---
+++ problem/day2.py
@@ -0,0 +1,20 @@
+# I will practice List of python . the list as like as array .
+
+fruits = ['apple','banana','charry','dates']
+
+print(type(fruits)) #the list type is list
+
+print(len(fruits)) #get len of list or we get how much element into list
+
+print(fruits[1]) #we can access list through the index. the index will be start from 0. the first index number is 0 of any list
+
+print(fruits[-1]) #negative number indicate last of the element of list
+
+print(fruits[2:4])
+
+
+#loop in list ,, we can iterate into list 
+
+for fruit in fruits:
+    print(fruit)
+
---
+++ problem/day3.py
@@ -0,0 +1,7 @@
+def checkNumber(x):
+  if x % 2 == 0:
+    print(f"The {x} is even")
+  else:
+    print(f"The {x} is odd")
+    
+checkNumber(5)
